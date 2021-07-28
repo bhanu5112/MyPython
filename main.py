98153751b53964b9a3b20e2b3862f1431ca661c7
bhanu5112 <83585623+bhanu5112@users.noreply.github.com>
2021-07-28 02:47:49 +0000

    user input

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 x=input()
-'''x=x.lower()
+x=x.lower()
 for i in ',.;!:':
   x=x.replace(i,"")
-x=x.split(" ")'''
+x=x.split(" ")
 
 def freqWords():
-  x = ['It', 'was', 'Monday', 'morning.', 'Swaminathan', 'was','reluctant', 'to', 'open', 'his', 'eyes.', 'He', 'considered', 'Monday', 'specially', 'unpleasant', 'in', 'the', 'calendar.', 'After', 'the', 'delicious', 'freedom']
   fqwords={}
   for i in x:
     c=x.count(i)
-    print(c)
     if c not in fqwords:
       fqwords[c]=[]
-     
     if i not in fqwords[c]:
       fqwords[c].append(i)
   return fqwords
